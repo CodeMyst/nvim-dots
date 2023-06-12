06e984cd1aa0a8b2701dbada048ac6d17de55e0a
CodeMyst <code@myst.rs>
2023-06-12 15:10:41 +0200

    keep selection after indenting

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -10,6 +10,9 @@ vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>', { desc = 'Toggle nvi
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set('v', "<", "<gv")
+vim.keymap.set('v', ">", ">gv")
+
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
