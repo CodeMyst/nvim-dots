fd40d2dd7046b2801fdaaca3f1353274f62f5313
CodeMyst <code@myst.rs>
2024-01-26 10:15:00 +0100

    enabled netrw so :GBrowser works

--- init.lua
+++ init.lua
@@ -58,8 +58,8 @@ vim.g.loaded_vimball           = 1
 vim.g.loaded_vimballPlugin     = 1
 vim.g.loaded_zip               = 1
 vim.g.loaded_zipPlugin         = 1
-vim.g.loaded_netrw             = 1
-vim.g.loaded_netrwPlugin       = 1
+-- vim.g.loaded_netrw             = 1
+-- vim.g.loaded_netrwPlugin       = 1
 
 vim.g.laststatus = 3
 
